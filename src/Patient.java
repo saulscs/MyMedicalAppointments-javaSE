f33b83f2873ad2bb05fbc0ece9ca24fa00dca10f
Saul Corona <saulcoronascs@gmail.com>
2021-07-21 21:31:04 -0500

    Clase paciente

---
+++ src/Patient.java
@@ -0,0 +1,17 @@
+public class Patient {
+    String name;
+    String email;
+    String address;
+    String phoneNumer;
+    String birthday;
+    Double weight;
+    Double height;
+    String blood;
+
+    //Constructor
+    public Patient(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+
+}
