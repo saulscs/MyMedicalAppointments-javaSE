f937146baf0d8186cd95d5e8e3597541d7396b9b
Saul Corona <saulcoronascs@gmail.com>
2021-07-21 19:22:56 -0500

    Creando primera clase (Doctor)

---
+++ src/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args){
+        Doctor myDoctor = new Doctor();
+        myDoctor.name = "Alejandro Rodriguez";
+        myDoctor.showName();
+    }
+}
